e874a6501253d3919cb4a37b37ffd4a6ebce7be1
Iliya <59892694+IHosseini083@users.noreply.github.com>
2021-06-26 17:18:43 +0430

    add changelog support

--- Modules/checkUpdates.py
+++ Modules/checkUpdates.py
@@ -6,7 +6,6 @@ from requests import get
 class CheckUpdates:
     def __init__(self) -> None:
         self.metadata_file = "metadata.json"
-        self.current_version = self.get_version
 
     @property
     def new_update(self):
@@ -16,21 +15,23 @@ class CheckUpdates:
             req = loads(req)
             github_ver = req["metadata"]["version"]
             str_github_ver = str(github_ver).replace(".", "")
-            str_current_ver = str(self.current_version).replace(".", "")
-            if github_ver != self.current_version:
+            str_current_ver = str(self.get_info[0]).replace(".", "")
+            changelog = self.get_info[1]
+            if github_ver != self.get_info[0]:
                 if int(str_github_ver) < int(str_current_ver):
                     return False
                 else:
-                    return github_ver
+                    return github_ver, changelog
             else:
                 return False
         except Exception:
             return None
 
     @property
-    def get_version(self):
+    def get_info(self):
         with open(self.metadata_file, "r") as meta_file:
             content = meta_file.read()
-            ver = loads(content)
-            ver = ver["metadata"]["version"]
-            return ver
+            info = loads(content)
+            ver = info["metadata"]["version"]
+            changelog = info["metadata"]["changelog"]
+            return ver, changelog
